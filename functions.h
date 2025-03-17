1456dbb39ba9e6cf48ecbb79aa2c953297031f36
anotherholeee <roikoartem@gmail.com>
2025-03-17 11:30:30 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,10 +1,9 @@
-
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-void write_to_file(const char *filename);
-void read_from_file(const char *filename);
-void find_min_word(const char *filename);
-void swap_first_two_words(const char *filename);
+void inputfile(const char *filename);
+void outputfile(const char *filename);
+void sum_numbers(const char *filename);
+void swap_spaces(const char *filename);
 
 #endif
